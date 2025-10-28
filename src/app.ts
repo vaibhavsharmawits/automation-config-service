3168dd131722fa6d9f9f19b25c9f22f30c3ea359
extedcouD <extedcoud@gmail.com>
2025-10-28 11:26:37 +0530

    Enhance configuration management with in-memory caching and add Jest testing framework

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,21 @@
 import express from "express";
 import cors from "cors";
-import { RedisService } from "ondc-automation-cache-lib";
 import * as path from "path";
 
 import routes from "./routes";
 import { loadYAMLWithRefs } from "./services/configService";
-import { setConfigService,getConfigService,deleteConfigService } from "./services/cacheService";
+import { getConfigService } from "./services/cacheService";
 
 const configPath = "./config/index.yaml";
-
-RedisService.useDb(parseInt(process.env.REDIS_DB as string) || 4);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-deleteConfigService().then(()=> getConfigService().then((config)=>{
+getConfigService().then((config)=>{
    console.log("config loaded from yamls")
-})).catch(console.error)
+}).catch(console.error)
 
 app.use(routes);
 
